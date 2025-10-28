a06c38c3f4a519de174d84ad2d042180e485cc16
lt-winter <minhdong9304@gmail.com>
2025-10-28 10:44:12 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const contactRoutes = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,6 +13,18 @@ app.get("/", (req, res) => {
     message: "Welcome to contact book application",
   });
 });
-app.use("/api/contacts", contactRoutes);
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
